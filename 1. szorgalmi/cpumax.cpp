fdb1b68b3e89b839ff1e2c4f6b8b485fc350b472
lordned110 <62188726+lordned110@users.noreply.github.com>
2025-04-23 18:24:40 +0200

    Update cpumax.cpp

--- 1. szorgalmi/cpumax.cpp
+++ 1. szorgalmi/cpumax.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <cmath>
 
+void cpumax() {
+    volatile double x = 0.0001;
+    while (true) {
+        x *= x;
+        x = sqrt(x);
+    }
+}
+
+int main() {
+    std::vector<std::thread> threads;
+    for (unsigned int i = 0; i < cpu_count; ++i) {
+        threads.emplace_back(cpumax);
+    }
+
+    for (auto& t : threads) t.join();
+    return 0;
+}
