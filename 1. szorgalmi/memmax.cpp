558f6bc1bf0661ed840b4e092931c56543060602
lordned110 <62188726+lordned110@users.noreply.github.com>
2025-04-23 18:30:25 +0200

    Update memmax.cpp

--- 1. szorgalmi/memmax.cpp
+++ 1. szorgalmi/memmax.cpp
@@ -1,1 +1,16 @@
-
+#include <vector>
+int main()
+{
+    struct mem
+    {
+        long double ram;
+    };
+    std::vector<mem> v;
+    while (true)
+    {
+        mem k;
+        k.ram = 1;
+        v.push_back(k);
+    }
+  return 0;
+}
