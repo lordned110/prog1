4c190f346db3fdeb7e60ff1e03973651d01e7bb6
lordned110 <62188726+lordned110@users.noreply.github.com>
2025-04-25 19:06:50 +0200

    Create s.cpp

---
+++ 7. szorgalmi/s.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;int main(){string s,r;getline(cin,s);for(char c:s)if(isalpha(c))r+=tolower(c);cout<<(r==string(r.rbegin(),r.rend())?"P\n":"N\n");int n;cin>>n;vector<int>v(n);for(int&x:v)cin>>x;sort(v.begin(),v.end());for(int x:v)cout<<x<<' ';}
