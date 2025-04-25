9f8ecaeccfa34e9b312f8a7c9b4b238067c794bb
lordned110 <62188726+lordned110@users.noreply.github.com>
2025-04-25 19:03:36 +0200

    Create vasarlas.cpp

---
+++ 6. szorgalmi/vasarlas.cpp
@@ -0,0 +1,35 @@
+#include <fstream>
+#include <map>
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+map<string, int> buy;
+int main(int argc, char* argv[])
+{
+	ifstream fin("lista.txt");
+	if (!fin.is_open()) {
+		cout << "Hiba: nem sikerült megnyitni a lista.txt fájlt!" << endl;
+		return 1;
+	}
+	string n;
+	int max=0;
+	while (getline(fin, n))
+	{
+		stringstream ss(n);
+		string darab, termek;
+		getline(ss, darab, ' ');
+		getline(ss, termek, ' ');
+		int db;
+		db = stoi(darab);
+		if (max < db) max=db;
+		if (buy[termek]) buy[termek] += 1;
+		else buy[termek] = 1;
+	}
+ofstream fout("output.txt");
+
+for (auto x : buy)
+fout << x.first << " " << x.second * 100 / max << "%\n";
+	return 0;
+}
