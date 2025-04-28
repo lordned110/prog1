775a77b2da1670d30c2d250743db7b2fa829d34b
lordned110 <62188726+lordned110@users.noreply.github.com>
2025-04-28 16:36:38 +0200

    Update bit.cpp

--- 7. szorgalmi/bit.cpp
+++ 7. szorgalmi/bit.cpp
@@ -1,5 +1,2 @@
-#include<iostream>
-#include<string>
-#include<vector>
-#include<algorithm>
+#include<bits/stdc++.h>
 using namespace std;int main(){string s,r;getline(cin,s);for(char c:s)if(isalpha(c))r+=tolower(c);cout<<(r==string(r.rbegin(),r.rend())?"P\n":"N\n");int n;cin>>n;vector<int>v(n);for(int&x:v)cin>>x;sort(v.begin(),v.end());for(int x:v)cout<<x<<' ';}
